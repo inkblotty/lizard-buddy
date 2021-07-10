ad8d0ca7bde204af5c389590403b98a2eb86c625
Katie Foster <fosterkatiea@gmail.com>
2021-07-09 23:41:56 -0500

    createPullRequest tests and some more setup

---
+++ server/src/handleGitBranch.ts
@@ -0,0 +1,13 @@
+import { Octokit } from "@octokit/core";
+
+const logColor = '#FCFF6C';
+
+// create new branch
+
+// parseThemeObjects
+
+// write these files to this branch
+
+// push all changes to branch
+
+// pass along branch name to createPullRequest
---
+++ server/src/parseThemeObjects.ts
@@ -0,0 +1,1 @@
+const logColor = '#4EA5D9';
