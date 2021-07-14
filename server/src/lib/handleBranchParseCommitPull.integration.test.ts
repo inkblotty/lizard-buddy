50f2630f654612a2fabf7455e58deea4382d0598
Katie Foster <fosterkatiea@gmail.com>
2021-07-13 22:19:33 -0500

    commenting

--- server/src/lib/handleBranchParseCommitPull.integration.test.ts
+++ server/src/lib/handleBranchParseCommitPull.integration.test.ts
@@ -6,8 +6,8 @@ const categories: (keyof BaseTheme)[] = ['colors', 'elevations', 'radii', 'spaci
 const mockContent = 'Hello world';
 const mockBranch = 'test-branch-123';
 
-describe('writeToTheme', () => {
-  test.each(categories)('creates a new %p file if category is %p', async (category: keyof BaseTheme) => {
-    await writeToTheme('BB', mockContent, { category, branch: mockBranch });
-  });
+describe('everything without the server', () => {
+  // test.each(categories)('creates a new %p file if category is %p', async (category: keyof BaseTheme) => {
+  //   await writeToTheme('BB', mockContent, { category, branch: mockBranch });
+  // });
 });
