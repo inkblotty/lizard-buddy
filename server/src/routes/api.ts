6942df5796fa3006d792985bfd4bbc9183c6df0b
Katie Foster <fosterkatiea@gmail.com>
2021-08-10 13:27:23 -0500

    switch to formik for consistency

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -4,18 +4,22 @@ import { getThemeIfExists } from '../lib/validators';
 
 export const register = (app: express.Application) => {
   app.post('/api/create/:code', async (req: express.Request, res: express.Response) => {
-    // validate if the code is already used
-    const { code } = req.params;
-    const existingTheme = getThemeIfExists(code);
-    if (existingTheme && !existingTheme.isDefaultTheme) {
-      // TODO: allow edits in another route
-      throw new Error(`Theme: ${code} already exists.`);
+    try {
+      // validate if the code is already used
+      const { code } = req.params;
+      const existingTheme = getThemeIfExists(code);
+      if (existingTheme && !existingTheme.isDefaultTheme) {
+        // TODO: allow edits in another route
+        throw new Error(`Theme: ${code} already exists.`);
+      }
+      
+      // validate that we don't already have too many PRs to review (come back later message)
+  
+      // now send along to handleBranchParseCommitPull
+      return res.json(handleBranchParseCommitPull(code, req.body));
+    } catch (err) {
+      return res.status(500).send(err);
     }
-    
-    // validate that we don't already have too many PRs to review (come back later message)
-
-    // now send along to handleBranchParseCommitPull
-    return res.json(handleBranchParseCommitPull(code, req.body));
   });
 
   app.get('/api/:code', async (req: express.Request, res: express.Response) => {
