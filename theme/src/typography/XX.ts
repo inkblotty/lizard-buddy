506eb6a9140487d11cc556e05b1a48004906729f
Katie Foster <fosterkatiea@gmail.com>
2021-08-27 12:45:10 -0500

    updates to all existing categories

--- theme/src/typography/XX.ts
+++ theme/src/typography/XX.ts
@@ -3,16 +3,68 @@ import { Typography } from "@lb/shared";
 /** used only for testing */
 const XXTestTypography: Typography = {
   body: {
-    main: 'Arial',
-    bold: 'Arial',
+    main: {
+      family: 'Arial',
+      lineHeight: '16px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    large: {
+      family: 'Arial',
+      lineHeight: '24px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    small: {
+      family: 'Arial',
+      lineHeight: '10px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    caption: {
+      family: 'Arial',
+      lineHeight: '12px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
   },
   headings: {
-    main: 'Arial',
-    bold: 'Arial',
-  },
-  alternate: {
-    main: 'Arial',
-    bold: 'Arial',
+    h1: {
+      family: 'Arial',
+      lineHeight: '48px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    h2: {
+      family: 'Arial',
+      lineHeight: '32px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    h3: {
+      family: 'Arial',
+      lineHeight: '24px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    h4: {
+      family: 'Arial',
+      lineHeight: '16px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    h5: {
+      family: 'Arial',
+      lineHeight: '14px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
+    h6: {
+      family: 'Arial',
+      lineHeight: '12px',
+      weight: 400,
+      familyUrls: ['/assets/somewhere'],
+    },
   },
 };
 export default XXTestTypography;
